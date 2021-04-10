4b618ddcab9000a6df32d0c395497482baae69c0
dshore-aspen <derek.shore@aspeninstitute.org>
2021-04-09 21:50:12 -0400

    Update for User Folders
    Updated to include a check against all relevant user folders and report on the findings: what was found and deleted versus what was missing

--- RemoveAdobeAIR-manual.sh
+++ RemoveAdobeAIR-manual.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-deletedFiles=()
+touch ~/Desktop/test/1\ test.txt
+touch ~/Desktop/test/2.txt
+
+
+allUsers=$(dscl . -ls /Users)
+list=($allUsers)
+realUsers=()
+for i in ${!list[@]}; 
+do
+
+	case ${list[$i]} in
+	"daemon")
+		;;
+	"nobody")
+		;;
+	"root")
+		;;
+	*)
+		if [[ ${list[$i]:0:1} != "_" ]]; then
+			echo "[USER ADD]----${list[$i]}"
+			realUsers+=(${list[$i]})
+		fi
+	esac	
+done
+echo "[USER LIST]----${realUsers[*]}"
+
+
+userFiles=(
+Desktop/test/1\ test.txt
+Desktop/test/2.txt
+Library/Application\ Support/Adobe/AIR/
+Library/Caches/com.adobe.air.Installer
+Library/Preferences/com.adobe.air.els.*
+)
 airFiles=(
 "/Applications/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller"
 "/Applications/Utilities/Adobe AIR Uninstaller.app"
@@ -13,38 +46,29 @@ airFiles=(
 "/Library/Frameworks/Adobe AIR.framework/Versions/1.0/Adobe AIR Application Installer.app"
 "/Library/Frameworks/Adobe AIR.framework/Versions/1.0/Adobe AIR/"
 "/Users/Shared/Library/Application Support/Adobe/AIR/"
-"~/Library/Application\ Support/Adobe/AIR/"
-"~/Library/Caches/com.adobe.air.Installer"
-"~/Library/Preferences/com.adobe.air.els.<some-hash>.dat.plist"
-"~/Library/Preferences/com.adobe.air.els.<some-hash>.dat.plist.lockfile"
 )
 
 
+echo "----------------"
+echo "----------------"
+echo "[BEGINNING]----Beginning to check for files"
+echo "----------------"
+
 for i in ${!airFiles[@]};
 do
-    target=${airFiles[$i]}
-    rm -rf $target && echo "[DELETING]----Deleting $target"
-    if [ -f $target ]; then
-        deletedFiles+=($target)
-        airFiles[$i]=""
-        echo "[CHECKING]----Successfully deleted $target"
-    else
-        echo "[CHECKING]----There was an error deleting $target"
-    fi
+		rm -r "${airFiles[$i]}" && echo "[FOUND]----${airFiles[$i]}"  ||  echo "[MISSING]----${airFiles[$i]}"
+done
+
+
+for u in ${!realUsers[@]};
+do
+	user=${realUsers[$u]}
+	echo "[USER]----$user"
+	for i in ${!userFiles[@]};
+	do
+		rm "/Users/$user/${userFiles[$i]}" && { echo "[FOUND]----${userFiles[$i]}"; deletedFiles+=${userFiles[$i]}; } || { echo "[MISSING]----${userFiles[$i]}"; deletedFiles+=${userFiles[$i]}; }
+	done
 done
 
 echo "----------------"
-echo "[REPORT]----The following files were discovered and deleted:"
-for i in ${deletedFiles[@]};
-do
-    echo "[REPORT]----deleted $i"
-done
-
 echo "----------------"
-echo "[REPORT]----The following files were not discovered or could not be deleted:"
-for i in ${airFiles[@]};
-do
-    if [ $i != "" ]; then 
-        echo "[REPORT]----deleted $i"
-    fi
-done
