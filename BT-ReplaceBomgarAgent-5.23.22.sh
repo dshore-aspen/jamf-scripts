a2f613ff4aee66f8f4667b95500e3afd76043e53
dshore-aspen <dwshore@me.com>
2022-05-23 14:41:03 -0400

    Updated downloader type and run commands.

--- BT-ReplaceBomgarAgent-5.23.22.sh
+++ BT-ReplaceBomgarAgent-5.23.22.sh
@@ -3,7 +3,6 @@
 mkdir -p /Library/Management/bomgar/
 cd /Library/Management/bomgar/
 
-curl -O btagent.dmg https://aspeninstitute.beyondtrustcloud.com/download_client_connector?fn=null&jc=39e41f307d2659bf22b82dfd401e8bf3&p=mac-osx-x86-dmg&ss=291e8ca1a9fb054bff729f0897987ff8dfe29e3c
-hdiutil attach /Library/Management/bomgar/bomgar-scc.dmg
-
-/bomgar-scc/Double-Click\ To\ Start\ Support\ Session.app/Contents/
+curl --output 'btagent.zip' "https://aspeninstitute.beyondtrustcloud.com/download_client_connector?fn=null&jc=918c8ddcbe4eec577d9a7862ee717cb8&p=mac-osx-x86&ss=cf588b6349ea900c7c31f87f599e2e7e35b4de4f"
+unzip ./btagent.zip
+./Open\ To\ Deploy\ Jump\ Client.app/Contents/MacOS/Bootstrap 
