e26ac9c2610a34bd698a41a08087ad559eb9a5a4
dshore-aspen <derek.shore@aspeninstitute.org>
2022-05-23 13:46:18 -0400

    Added dmg mount command. (still WIP)

--- BT-ReplaceBomgarAgent-5.23.22.sh
+++ BT-ReplaceBomgarAgent-5.23.22.sh
@@ -4,5 +4,6 @@ mkdir -p /Library/Management/bomgar/
 cd /Library/Management/bomgar/
 
 curl -O btagent.dmg https://aspeninstitute.beyondtrustcloud.com/download_client_connector?fn=null&jc=39e41f307d2659bf22b82dfd401e8bf3&p=mac-osx-x86-dmg&ss=291e8ca1a9fb054bff729f0897987ff8dfe29e3c
+hdiutil attach /Library/Management/bomgar/bomgar-scc.dmg
 
-/bomgar-scc/Double-Click\ To\ Start\ Support\ Session.app
+/bomgar-scc/Double-Click\ To\ Start\ Support\ Session.app/Contents/
